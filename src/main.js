778aea7eff5e41feeae327ccb1b491a024e435d5
1352310219@qq.com <1352310219@qq.com>
2018-12-14 14:09:59 +0800

    1

--- src/main.js
+++ src/main.js
@@ -18,11 +18,11 @@ Vue.use(VueAwesomeSwiper)
 // Vue.prototype.$echarts = echarts 
 window.eventBus = new Vue()
 if(localStorage.getItem('socketPort')){
-	Vue.use(VueSocketio, 'http://lever.mobile369.com:' + localStorage.getItem('socketPort'));
+	Vue.use(VueSocketio, 'http://www.bitex.ren:' + localStorage.getItem('socketPort'));
 }else{
 	Axios.get('/api/env.json').then(res=>{
 		localStorage.setItem('socketPort',res.data.socket_io_port);
-		Vue.use(VueSocketio, 'http://lever.mobile369.com:' + res.data.socket_io_port);
+		Vue.use(VueSocketio, 'http://www.bitex.ren:' + res.data.socket_io_port);
 	})
 }
 // Vue.use(VueSocketio, 'http://ice.adminchao.com:2120');
